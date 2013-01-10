57f3a125ee18707e1e7f1d15f07370886ec49b1b
Tony Hariman <thariman@gmail.com>
2013-01-10 23:29:06 +0700

    increase memory and core

--- vmcompute.sh
+++ vmcompute.sh
@@ -4,7 +4,7 @@ VM_NAME="compute1"
 VM_DIR='/Users/thariman/VirtualBox VMs/'$VM_NAME
 
 VBoxManage createvm --name $VM_NAME --register
-VBoxManage modifyvm "$VM_NAME" --memory 2048 --cpus 2 --boot1 disk --boot2 net --boot3 none --boot4 none
+VBoxManage modifyvm "$VM_NAME" --memory 4096 --cpus 4 --boot1 disk --boot2 net --boot3 none --boot4 none
 VBoxManage modifyvm "$VM_NAME" --audio none --ostype Ubuntu_64 --vram 16 --acpi on --usb on --usbehci on --ioapic on
 VBoxManage modifyvm "$VM_NAME" --nic1 hostonly --nictype1 82540EM --hostonlyadapter1 vboxnet0 --macaddress1 0800273A02DE
 VBoxManage modifyvm "$VM_NAME" --nic2 intnet --nictype2 82540EM --macaddress2 08002777EF08
